3d8c588fac1d4a20cd17b829df0aec372e2a0aa2
Nathanael Praditya <leerlryzck9@gmail.com>
2025-11-01 09:37:49 +0700

    batuguntingkertas.c

--- c/batuguntingkertas.c
+++ c/batuguntingkertas.c
@@ -1,2 +1,4 @@
 //   - Buat permainan batu-gunting-kertas melawan komputer
 //   - Gunakan random number generator untuk pilihan komputer
+//   - Tampilkan hasil setiap ronde
+//   - Berikan opsi untuk bermain ulang
