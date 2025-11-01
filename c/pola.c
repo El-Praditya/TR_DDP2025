2fb44941787132c6646a83607c26a47758e8c425
Nathanael Praditya <leerlryzck9@gmail.com>
2025-11-01 09:36:49 +0700

    pola.c

---
+++ c/pola.c
@@ -0,0 +1,5 @@
+// Pola grafis rumah
+//   - Buat pola rumah menggunakan karakter ASCII
+//   - Input: tinggi dan karakter penyusun
+//   - Output: pola rumah dengan minimal 3 bagian berbeda
+//   - Gunakan warna berbeda untuk setiap bagian
