4f63e4696545e1eb555a68c1753e5581d2397cd1
Nathanael Praditya <leerlryzck9@gmail.com>
2025-11-01 09:38:45 +0700

    kamus.c

---
+++ c/kamus.c
@@ -0,0 +1,4 @@
+ //  - Buat sistem kamus dengan input dari user
+ //  - Fitur: tambah kata dan definisi
+ //  - Implementasikan pencarian kata
+ //  - Tampilkan hasil pencarian dengan format yang rapi
