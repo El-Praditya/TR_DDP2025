e6ba135e3031d30fb59809f3decacb1ac101121f
Nathanael Praditya <leerlryzck9@gmail.com>
2025-11-16 22:25:33 +0700

    Add files via upload

---
+++ SourceCode/author.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main (){
+
+    system("cls");
+    Sleep(900);
+
+    printf("\033[33m\n[INFO]\033[0m Retrieving Name...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Retrieving Student ID...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Retrieving GitHub Profile...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Retrieving Personal Quote...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Rendering profile...\n");
+    Sleep(2600);
+
+    printf("\033[32m[SUCCESS]\033[0m Operation successful...\n");
+    Sleep(4200);
+
+    printf("\033[32m[SUCCESS]\033[0m System online...\n\n");
+    Sleep(1300);
+
+    printf("\033[31m    ___    __                __     __  _______\n");
+    printf("   /   |  / /_  ____  __  __/ /_   / / / / ___/\n");
+    printf("  / /| | / __ \\/ __ \\/ / / / __/  / / / /\\__ \\ \n");
+    printf(" / ___ |/ /_/ / /_/ / /_/ / /_   / /_/ /___/ / \n");
+    printf("/_/  |_/_.___/\\____/\\__,_/\\__/   \\____//____/\033[0m  \n\n");
+    printf("\033[31m====================KELOMPOK 3.0=====================\033[0m\n");
+ 
+    printf("\n\n\033[32m=====================================================\033[0m\n");
+    printf("\033[31mDEVELOPER:\033[0m\t \033[32mTeofilus Tri Adinugraha Moelyono\033[0m \n");
+    printf("\033[31mNIM:\033[0m\t \t \033[32m672025093\033[0m \n");
+    printf("\033[31mGITHUB: \033[0m\t \033[32mgithub.com/tridelta102\033[0m \n");
+    printf("\033[31mQUOTE:\033[0m \t \t \033[32mGapapa. Aku tetap happy saja\033[0m\n");
+    printf("\033[32m-----------------------------------------------------\033[0m\n");
+    printf("\033[31mDEVELOPER:\033[0m\t \033[32mHanny Sokha Anggana\033[0m \n");
+    printf("\033[31mNIM:\033[0m\t \t \033[32m672025096\033[0m \n");
+    printf("\033[31mGITHUB:\033[0m \t \033[32mgithub.com/Sokhatok17\033[0m \n");
+    printf("\033[31mQUOTE: \033[0m\t \t \033[32mTak pendem e dewe wae gen dadi telo\033[0m\n");
+    printf("\033[32m-----------------------------------------------------\033[0m\n");
+    printf("\033[31mDEVELOPER:\033[0m\033[0m\t \033[32mNathanael Afarrel Praditya\033[0m \n");
+    printf("\033[31mNIM:\033[0m\t \t \033[32m672025101\033[0m \n");
+    printf("\033[31mGITHUB:\033[0m \t \033[32mgithub.com/El-Praditya\033[0m \n");
+    printf("\033[31mQUOTE:\033[0m \t \t \033[32mI Love My Family\033[0m\n");
+    printf("\033[32m=====================================================\033[0m\n\n");
+
+    return 0;
+    
+}
