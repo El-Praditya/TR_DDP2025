661fb1279e3a46cff751399a727fa254f0105a78
teo <tridelta102@gmail.com>
2025-11-17 08:45:20 +0700

    Update main code.c
    done!

--- SourceCode/main code.c
+++ SourceCode/main code.c
@@ -11,6 +11,7 @@ int kamus();
 void pola();
 void game();
 void yesno();
+void author();
 void konfirmasikeluar();
 
 int loadonce = 0;
@@ -164,10 +165,12 @@ int pilihan;
         game();
         } else if(pilihan == 4){
         kamus();
-        } else if (pilihan == 5){
-            konfirmasikeluar();
-            break;
-        } 
+        } else if(pilihan == 5){
+        konfirmasikeluar();
+        } else if(pilihan == 6){
+        author();
+        break;
+        }
     }
     while (pilihan < 1 || pilihan > 6);
 
@@ -560,6 +563,62 @@ void konfirmasikeluar(){
                  printf("\033[31m\n[ALERT]\033[0m Pilihan '%c' tidak valid. Pilih 'y' atau 'n' :", konfir);
                  konfirmasikeluar();
             }
+}
+
+void author(){
+
+    system("cls");
+    Sleep(900);
+
+    printf("\033[33m\n[INFO]\033[0m Retrieving Name...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Retrieving Student ID...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Retrieving GitHub Profile...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Retrieving Personal Quote...\n");
+    Sleep(1600);
+
+    printf("\033[33m[INFO]\033[0m Rendering profile...\n");
+    Sleep(2600);
+
+    printf("\033[32m[SUCCESS]\033[0m Operation successful...\n");
+    Sleep(4200);
+
+    printf("\033[32m[SUCCESS]\033[0m System online...\n\n");
+    Sleep(1300);
+
+    printf("\033[31m    ___    __                __     __  _______\n");
+    printf("   /   |  / /_  ____  __  __/ /_   / / / / ___/\n");
+    printf("  / /| | / __ \\/ __ \\/ / / / __/  / / / /\\__ \\ \n");
+    printf(" / ___ |/ /_/ / /_/ / /_/ / /_   / /_/ /___/ / \n");
+    printf("/_/  |_/_.___/\\____/\\__,_/\\__/   \\____//____/\033[0m  \n\n");
+    printf("\033[31m====================KELOMPOK 3.0=====================\033[0m\n");
+ 
+    printf("\n\n\033[32m=====================================================\033[0m\n");
+    printf("\033[31mDEVELOPER:\033[0m\t \033[32mTeofilus Tri Adinugraha Moelyono\033[0m \n");
+    printf("\033[31mNIM:\033[0m\t \t \033[32m672025093\033[0m \n");
+    printf("\033[31mGITHUB: \033[0m\t \033[32mgithub.com/tridelta102\033[0m \n");
+    printf("\033[31mQUOTE:\033[0m \t \t \033[32mGapapa. Aku tetap happy saja\033[0m\n");
+    printf("\033[32m-----------------------------------------------------\033[0m\n");
+    printf("\033[31mDEVELOPER:\033[0m\t \033[32mHanny Sokha Anggana\033[0m \n");
+    printf("\033[31mNIM:\033[0m\t \t \033[32m672025096\033[0m \n");
+    printf("\033[31mGITHUB:\033[0m \t \033[32mgithub.com/Sokhatok17\033[0m \n");
+    printf("\033[31mQUOTE: \033[0m\t \t \033[32mTak pendem e dewe wae gen dadi telo\033[0m\n");
+    printf("\033[32m-----------------------------------------------------\033[0m\n");
+    printf("\033[31mDEVELOPER:\033[0m\033[0m\t \033[32mNathanael Afarrel Praditya\033[0m \n");
+    printf("\033[31mNIM:\033[0m\t \t \033[32m672025101\033[0m \n");
+    printf("\033[31mGITHUB:\033[0m \t \033[32mgithub.com/El-Praditya\033[0m \n");
+    printf("\033[31mQUOTE:\033[0m \t \t \033[32mI Love My Family\033[0m\n");
+    printf("\033[32m=====================================================\033[0m\n\n");
+
+    printf("Tekan enter untuk kembali ke menu...");
+    getchar();
+    getchar();
+    menu();
 }
 
 //fungsi yang dipanggil setiap ada pilihan yes/no [y/n]
